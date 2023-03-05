b11fa0896a5fc99340adfd8e7a46315bd0bab715
AliHussainWaris <mohammedalihussainwaris@gmail.com>
2023-03-05 22:30:04 +0500

    User Login

---
+++ routes/User/Login/UserLogin.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+const UserLogin = require('../../../api/controller/User/Login/UserLogin')
+
+router.post("/register" , UserLogin.adddata)
+router.get("/read",UserLogin.getdata)
+router.get("/single",UserLogin.getonedata)
+router.put("/update/:email",UserLogin.updatadata)
+router.delete("/delete/:email",UserLogin.deletedata)
+
+module.exports = router
--- server.js
+++ server.js
@@ -7,12 +7,14 @@ const bodyParser = require('body-parser')
 require('./db/db')
 
 const AdminLogin = require("./routes/Admin/Login/AdminLogin")
+const UserLogin = require("./routes/User/Login/UserLogin")
 
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use("/Admin",AdminLogin)
+app.use("/User",UserLogin)
 
 app.listen(Port , ()=>{
     console.log(`Server Running on the PORT ${Port}`)
